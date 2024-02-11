d728190bbaccc9fe5deacbc35774b3caa1835e3b
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-02-11 17:25:26 +0100

    hw1 is done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,55 @@
-import { Feedback } from './Feedback/Feedback'
+import { Component } from "react"
+import styles from "./Feedback/Feedback.module.css"
+import { Statistics } from "./Feedback/Statistics"
+import { FeedbackOptions } from "./Feedback/FeedbackOptions"
+import { Section } from "./helpers/Section"
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Feedback/>
-    </div>
-  );
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  }
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state
+    return good + neutral + bad
+  }
+
+  countPositiveFeedbackPercentage() {
+    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2)
+  }
+
+  handlerAddRate = (feedbackType) => {
+    this.setState((prevState) => ({
+      [feedbackType] : prevState[feedbackType] + 1
+    }))
+  }
+
+  render() {
+    const { good, neutral, bad } = this.state
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Section title='Please leave feedback' children={
+          <div className={styles.button_wrapper}>
+              {Object.keys(this.state).map((key) => (
+                  <FeedbackOptions key={key} options={key} onLeaveFeedback={this.handlerAddRate} />))}
+          </div>} />
+          <Section title='Feedback statistics' children={
+            <Statistics good={good} neutral={neutral} bad={bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}/>
+          } />
+      </div>
+    );
+  }
 };
