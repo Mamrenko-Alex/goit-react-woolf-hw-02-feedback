4422c3272df74180aaab1ec6929939341ab48dc9
Mamrenko-Alex <Mamrenkodev@yandex.com>
2024-02-17 18:44:30 +0100

    Prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,33 @@
-import { Component } from "react"
-import { Statistics } from "./Statistics"
-import { FeedbackOptions } from "./FeedbackOptions"
-import { Section } from "./Section"
-import { Notification } from "./Notification"
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section';
+import { Notification } from './Notification';
 
 export class App extends Component {
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
-  }
+  };
 
   countTotalFeedback() {
-    const { good, neutral, bad } = this.state
-    return good + neutral + bad
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
   }
 
   countPositiveFeedbackPercentage() {
-    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2)
+    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2);
   }
 
-  handlerAddRate = (feedbackType) => {
-    this.setState((prevState) => ({
-      [feedbackType] : prevState[feedbackType] + 1
-    }))
-  }
+  handlerAddRate = feedbackType => {
+    this.setState(prevState => ({
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
 
   render() {
-    const { good, neutral, bad } = this.state
+    const { good, neutral, bad } = this.state;
 
     return (
       <div
@@ -38,21 +38,35 @@ export class App extends Component {
           flexDirection: 'column',
           alignItems: 'center',
           fontSize: 40,
-          color: '#010101'
+          color: '#010101',
         }}
       >
-        <Section title='Please leave feedback' children={
-          <FeedbackOptions options={Object.keys(this.state)} onLeaveFeedback={this.handlerAddRate} />
-        } />
-        {this.countTotalFeedback() > 0 ? <Section title='Feedback statistics' children={
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={this.countTotalFeedback()}
-            positivePercentage={this.countPositiveFeedbackPercentage()}/>} /> : <Notification message='There is no feedback'/>}
-        
+        <Section
+          title="Please leave feedback"
+          children={
+            <FeedbackOptions
+              options={Object.keys(this.state)}
+              onLeaveFeedback={this.handlerAddRate}
+            />
+          }
+        />
+        {this.countTotalFeedback() > 0 ? (
+          <Section
+            title="Feedback statistics"
+            children={
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={this.countTotalFeedback()}
+                positivePercentage={this.countPositiveFeedbackPercentage()}
+              />
+            }
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
       </div>
     );
   }
-};
+}
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,10 @@
-import styles from './FeedbackOptions.module.css'
+import styles from './FeedbackOptions/FeedbackOptions.module.css';
 
 export const Section = ({ title, children }) => {
-    return (
-        <section>
-            <h3 className={styles.headers}>{title}</h3>
-            {children}
-        </section>
-    );
+  return (
+    <section>
+      <h3 className={styles.headers}>{title}</h3>
+      {children}
+    </section>
+  );
 };
